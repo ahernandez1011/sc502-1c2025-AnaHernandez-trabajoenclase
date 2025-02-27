7671fd1282cc7074b10b3196ee591ecfbcdb53ff
Ana Hernández <analidia1025@gmail.com>
2025-02-27 11:04:11 -0600

    Cambios pequeños en holamundo.php

--- Semana6/php/holamundo.php
+++ Semana6/php/holamundo.php
@@ -7,7 +7,7 @@
     <?php
         echo "<p>Hola mundo en PHP</p>";
         // declaracion de variables
-        $nombre = "Diego";//variable global, puede ser accedida desde cualquier funcion
+        $nombre = "Diego ";//variable global, puede ser accedida desde cualquier funcion
         $contador = 9;
         $peso = 69.7;
         $mayorEdad = true;
@@ -24,25 +24,25 @@
         $edad = 15;
         //if simple
         if($edad < 18){
-            echo "eres menor de edad";
+            echo " eres menor de edad ";
         } elseif ($edad == 18) {
             echo "tienes 18";
         } else {
-            echo "eres mayor de edad";
+            echo " eres mayor de edad ";
             $mayorEdad = true;
         }
  
-        //aninados
+        //anidados
         $membresia = true;
         if($edad >= 18){
             echo " Adulto";
             if($membresia){
-               echo "  miembro";
+               echo "  miembro ";
             }
         } else {
             echo "menor de edad";
             if($membresia){
-                echo " y miembro";
+                echo " y miembro ";
             }
         }
  
