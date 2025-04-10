f931c8241a77adc65e1f322e92d11f66ebef5e05
Ana Hernández <analidia1025@gmail.com>
2025-04-10 12:41:09 -0600

    Implementación del GET TASKS en el UI

--- Semana12/app-tareas/js/dashboard.js
+++ Semana12/app-tareas/js/dashboard.js
@@ -1,47 +1,43 @@
 document.addEventListener('DOMContentLoaded', function () {
-
+ 
+    const API_URL = "backend/tasks.php";
     let isEditMode = false;
     let edittingId;
-    const tasks = [{
-        id: 1,
-        title: "Complete project report",
-        description: "Prepare and submit the project report",
-        dueDate: "2024-12-01",
-        comments: [{ id: 1, description: "There is too many tasks pending" }, { id: 2, description: "It needs to be a complete report" }]
-    },
-    {
-        id: 2,
-        title: "Team Meeting",
-        description: "Get ready for the season",
-        dueDate: "2024-12-01",
-    },
-    {
-        id: 3,
-        title: "Code Review",
-        description: "Check partners code",
-        dueDate: "2024-12-01",
-
-    },
-    {
-        id: 4,
-        title: "Deploy",
-        description: "Check deploy steps",
-        dueDate: "2024-12-01",
-    }];
-
-    function loadTasks() {
+    let tasks = [];
+ 
+    async function loadTasks() {
+        //go to the backed to obtain the data
+        try {
+            const response = await fetch(API_URL, { method: 'GET', credentials: 'include' });
+            if (response.ok) {
+                tasks = await response.json();
+                renderTasks(tasks);
+            } else {
+                if (response.status == 401) {
+                    //estamos tratando de consutlar sin sesion
+                    window.location.href = "index.html";
+                }
+                console.error("Error al obtener tareas");
+            }
+ 
+        } catch (err) {
+            console.error(err);
+        }
+    }
+ 
+    function renderTasks() {
         const taskList = document.getElementById('task-list');
         taskList.innerHTML = '';
         tasks.forEach(function (task) {
-
+ 
             let commentsList = '';
             if (task.comments && task.comments.length > 0) {
                 commentsList = '<ul class="list-group list-group-flush">';
                 task.comments.forEach(comment => {
-                    commentsList += `<li class="list-group-item">${comment.description} 
+                    commentsList += `<li class="list-group-item">${comment.description}
                     <button type="button" class="btn btn-sm btn-link remove-comment" data-visitid="${task.id}" data-commentid="${comment.id}">Remove</button>
                     </li>`;
-                }); 
+                });
                 commentsList += '</ul>';
             }
             const taskCard = document.createElement('div');
@@ -51,10 +47,10 @@ document.addEventListener('DOMContentLoaded', function () {
                 <div class="card-body">
                     <h5 class="card-title">${task.title}</h5>
                     <p class="card-text">${task.description}</p>
-                    <p class="card-text"><small class="text-muted">Due: ${task.dueDate}</small> </p>
+                    <p class="card-text"><small class="text-muted">Due: ${task.due_Date}</small> </p>
                     ${commentsList}
                      <button type="button" class="btn btn-sm btn-link add-comment"  data-id="${task.id}">Add Comment</button>
-
+ 
                 </div>
                 <div class="card-footer d-flex justify-content-between">
                     <button class="btn btn-secondary btn-sm edit-task"data-id="${task.id}">Edit</button>
@@ -64,22 +60,22 @@ document.addEventListener('DOMContentLoaded', function () {
             `;
             taskList.appendChild(taskCard);
         });
-
+ 
         document.querySelectorAll('.edit-task').forEach(function (button) {
             button.addEventListener('click', handleEditTask);
         });
-
+ 
         document.querySelectorAll('.delete-task').forEach(function (button) {
             button.addEventListener('click', handleDeleteTask);
         });
-
+ 
         document.querySelectorAll('.add-comment').forEach(function (button) {
             button.addEventListener('click', function (e) {
                 // alert(e.target.dataset.id);
                 document.getElementById("comment-task-id").value = e.target.dataset.id;
                 const modal = new bootstrap.Modal(document.getElementById("commentModal"));
-            modal.show()
-
+                modal.show()
+ 
             })
         });
         document.querySelectorAll('.remove-comment').forEach(function (button) {
@@ -88,19 +84,19 @@ document.addEventListener('DOMContentLoaded', function () {
                 let commentId = parseInt(e.target.dataset.commentid);
                 selectedTask = tasks.find(t => t.id === taskId);
                 commentIndex = selectedTask.comments.findIndex(c => c.id === commentId);
-                selectedTask.comments.splice(commentIndex,1);
+                selectedTask.comments.splice(commentIndex, 1);
                 loadTasks();
             })
         });
     }
-
+ 
     function handleEditTask(event) {
         try {
             // alert(event.target.dataset.id);
             //localizar la tarea quieren editar
             const taskId = parseInt(event.target.dataset.id);
             const task = tasks.find(t => t.id === taskId);
-            //cargar los datos en el formulario 
+            //cargar los datos en el formulario
             document.getElementById('task-title').value = task.title;
             document.getElementById('task-desc').value = task.description;
             document.getElementById('due-date').value = task.dueDate;
@@ -110,15 +106,15 @@ document.addEventListener('DOMContentLoaded', function () {
             //mostrar el modal
             const modal = new bootstrap.Modal(document.getElementById("taskModal"));
             modal.show();
-
-
+ 
+ 
         } catch (error) {
             alert("Error trying to edit a task");
             console.error(error);
         }
     }
-
-
+ 
+ 
     function handleDeleteTask(event) {
         // alert(event.target.dataset.id);
         const id = parseInt(event.target.dataset.id);
@@ -126,36 +122,36 @@ document.addEventListener('DOMContentLoaded', function () {
         tasks.splice(index, 1);
         loadTasks();
     }
-
-    document.getElementById('comment-form').addEventListener('submit', function (e){
+ 
+    document.getElementById('comment-form').addEventListener('submit', function (e) {
         e.preventDefault();
         const comment = document.getElementById('task-comment').value;
         const selectedTask = parseInt(document.getElementById('comment-task-id').value);
-        const task = tasks.find(t=> t.id === selectedTask);
-
-
+        const task = tasks.find(t => t.id === selectedTask);
+ 
+ 
         let nextCommentId = 1;
-         
-        if(task.comments){
+ 
+        if (task.comments) {
             nextCommentId = task.comments.length + 1;
-        }else{
+        } else {
             task.comments = [];
         }
-        
-        task.comments.push({id: nextCommentId, description: comment});
+ 
+        task.comments.push({ id: nextCommentId, description: comment });
         const modal = bootstrap.Modal.getInstance(document.getElementById('commentModal'));
         modal.hide();
         loadTasks();
-
+ 
     })
-
+ 
     document.getElementById('task-form').addEventListener('submit', function (e) {
         e.preventDefault();
-
+ 
         const title = document.getElementById("task-title").value;
         const description = document.getElementById("task-desc").value;
         const dueDate = document.getElementById("due-date").value;
-
+ 
         if (isEditMode) {
             //todo editar
             const task = tasks.find(t => t.id === edittingId);
@@ -175,11 +171,11 @@ document.addEventListener('DOMContentLoaded', function () {
         modal.hide();
         loadTasks();
     });
-
-    document.getElementById('commentModal').addEventListener('show.bs.modal', function(){
+ 
+    document.getElementById('commentModal').addEventListener('show.bs.modal', function () {
         document.getElementById('comment-form').reset();
     })
-
+ 
     document.getElementById('taskModal').addEventListener('show.bs.modal', function () {
         if (!isEditMode) {
             document.getElementById('task-form').reset();
@@ -188,11 +184,10 @@ document.addEventListener('DOMContentLoaded', function () {
             // document.getElementById('due-date').value = "";
         }
     });
-
+ 
     document.getElementById("taskModal").addEventListener('hidden.bs.modal', function () {
         edittingId = null;
         isEditMode = false;
     })
     loadTasks();
-
 });
